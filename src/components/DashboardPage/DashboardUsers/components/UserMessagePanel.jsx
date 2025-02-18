58c851124d9dfd17138997d480d6f2c1039b2f34
Andolon-M <aemendez-l@outlook.com>
2025-02-18 17:14:06 -0500

    feat: :lipstick: correccion de estilos responsive en mensajes masivos

--- src/components/DashboardPage/DashboardUsers/components/UserMessagePanel.jsx
+++ src/components/DashboardPage/DashboardUsers/components/UserMessagePanel.jsx
@@ -14,38 +14,37 @@ export const UserMessagePanel = ({
 }) => {
   return (
     <Card className="col-span-2 bg-slate-800/50 border-slate-700 h-full">
-  <div className="p-3 md:p-4 flex flex-col h-full">
-    <div className="flex-1 relative">
-      <div 
-        className={`absolute inset-0 bg-slate-800/80 backdrop-blur-sm z-10 flex items-center justify-center rounded-lg transition-all duration-300 ease-in-out ${
-          isLoading ? 'opacity-100' : 'opacity-0 pointer-events-none'
-        }`}
-      >
-        <div className="flex items-center space-x-2">
-          <Loader2 className="w-6 h-6 text-cyan-500 animate-spin" />
-          <span className="text-slate-200">Cargando usuarios...</span>
+      <div className="p-3 md:p-4 flex flex-col h-full">
+        <div className="flex-1 relative">
+          <div
+            className={`absolute inset-0 bg-slate-800/80 backdrop-blur-sm z-10 flex items-center justify-center rounded-lg transition-all duration-300 ease-in-out ${isLoading ? 'opacity-100' : 'opacity-0 pointer-events-none'
+              }`}
+          >
+            <div className="flex items-center space-x-2">
+              <Loader2 className="w-6 h-6 text-cyan-500 animate-spin" />
+              <span className="text-slate-200">Cargando usuarios...</span>
+            </div>
+          </div>
+          <div className={`transition-opacity duration-300 ${isLoading ? 'opacity-50' : 'opacity-100'}`}>
+            <UserList
+              selectedUsers={selectedUsers}
+              selectedTemplate={selectedTemplate}
+              stateSelected={stateSelected}
+            />
+          </div>
+        </div>
+        <div className="mt-4 md:mt-6 flex-shrink-0">
+          <SendMessageButton
+            selectedTemplate={selectedTemplate}
+            citySelected={citySelected}
+            stateSelected={stateSelected}
+            usersExist={selectedUsers.length}
+            isLoading={isLoading}
+            onSendMessages={onSendMessages}
+          />
         </div>
       </div>
-      <div className={`transition-opacity duration-300 ${isLoading ? 'opacity-50' : 'opacity-100'}`}>
-        <UserList
-          selectedUsers={selectedUsers}
-          selectedTemplate={selectedTemplate}
-          stateSelected={stateSelected}
-        />
-      </div>
-    </div>
-    <div className="mt-4 md:mt-6 flex-shrink-0">
-      <SendMessageButton
-        selectedTemplate={selectedTemplate}
-        citySelected={citySelected}
-        stateSelected={stateSelected}
-        usersExist={selectedUsers.length}
-        isLoading={isLoading}
-        onSendMessages={onSendMessages}
-      />
-    </div>
-  </div>
-</Card>
+    </Card>
   );
 };
 
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -4,12 +4,15 @@ import { Outlet } from 'react-router-dom';
 
 export const DashboardPage = () => {
   return (
-      <div className="h-screen flex flex-col lg:flex-row overflow-y-scroll lg:overflow-hidden bg-slate-900">
-        <DashboardNavbar />
-        <div className="flex-1 flex flex-col min-h-0">
-          <DashboardHeader />
+    <div className="h-screen flex flex-col lg:flex-row overflow-y-scroll lg:overflow-hidden bg-slate-900">
+      <DashboardNavbar />
+      <div className="flex-1 flex flex-col min-h-0 h-screen">
+        <DashboardHeader />
+        <div className='h-screen overflow-y-auto scrollbar-custom pb-5'>
           <Outlet />
+
         </div>
       </div>
+    </div>
   );
 };
